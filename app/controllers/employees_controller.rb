6a6a15c5eb3cf2adc7e71043ceefea4a4bed3346
Claire Wilbert <claire.wilbert@gmail.com>
2019-07-03 12:27:38 -0700

    First run through alone

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def create
+    @dogs = Dog.all
+    @employee = Employee.new(employee_params)
+    if @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def update
+    @dogs = Dog.all
+    @employee = Employee.new(employee_params)
+    if @employee.update
+      redirect_to employee_path(@employee)
+    else
+      render :update
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :office, :title, :dog_id)
+    
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+  validates :alias, uniqueness: true
+  validates :title, uniqueness: true
+
+  def full_name
+    self.first_name.to_s + " " + self.last_name.to_s
+  end
+
 end
